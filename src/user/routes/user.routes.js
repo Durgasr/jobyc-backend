e7f56d95d02a2fb55e75c5576b75f4cf8e0db60a
Durgasri <challadurgasri2000@gmail.com>
2025-10-02 16:45:41 +0530

    Add an api for user logout

--- src/user/routes/user.routes.js
+++ src/user/routes/user.routes.js
@@ -6,11 +6,13 @@ import {
 } from "../controller/user.controller.js";
 import { isAuthenticated } from "../../../middlewares/auth.js";
 import upload from "../../../middlewares/fileUpload.js";
+import { logoutUser } from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.route("/register").post(upload.single("resume"), createNewUser);
 router.route("/login").post(userLogin);
 router.route("/me").get(isAuthenticated, getUserDetails);
+router.route("/logout").post(isAuthenticated, logoutUser);
 
 export default router;
