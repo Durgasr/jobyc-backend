5cabe2bb56a6ef4edcf7ae77f402704df06e8484
Durgasri <challadurgasri2000@gmail.com>
2025-10-23 23:25:53 +0530

    Added condition to check progress score

--- src/jobs/controller/job.controller.js
+++ src/jobs/controller/job.controller.js
@@ -6,8 +6,9 @@ import {
   updateJobRepo,
   deleteJobRepo,
   findJobDetailsById,
-  findAllJobsRepo
+  findAllJobsRepo,
 } from "../models/job.repository.js";
+import { User } from "../../user/models/user.schema.js";
 
 export const createJob = async (req, res, next) => {
   try {
@@ -15,9 +16,23 @@ export const createJob = async (req, res, next) => {
       return res.status(403).json({ message: "Only recruiters can post jobs" });
     }
 
+    const recruiter = await User.findById(req.user.id);
+    if (!recruiter) {
+      return res.status(404).json({ message: "Recruiter not found" });
+    }
+
+    // ✅ Progress check
+    if (recruiter.progress < 80) {
+      return res.status(403).json({
+        message:
+          "Please complete your profile (minimum 80%) before posting a job.",
+      });
+    }
+
     const job = await createJobRepo({ ...req.body, recruiter: req.user.id });
     res.status(201).json({ success: true, job });
   } catch (err) {
+    console.log(err);
     next(new ErrorHandler(500, err));
   }
 };
@@ -60,12 +75,11 @@ export const deleteJob = async (req, res, next) => {
   }
 };
 
-export const getAllJobs = async (req,res,next)=>{
-  try{
+export const getAllJobs = async (req, res, next) => {
+  try {
     const allJobs = await findAllJobsRepo();
-    res.json({success:true, jobs:allJobs})
-  }
-  catch(err){
+    res.json({ success: true, jobs: allJobs });
+  } catch (err) {
     next(new ErrorHandler(500, err));
   }
-}
+};
