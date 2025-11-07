c13ee8af07e440a65cb940a83b1e89114b3d9a13
Durgasri <challadurgasri2000@gmail.com>
2025-11-08 03:49:59 +0530

    Hardcode CORS origin for testing

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL}` || "http://localhost:3000",
+    origin: "https://jobyccareers.netlify.app",
     credentials: true,
   })
 );
